375a413f1b75b2f62ed1c40795ad852e5951ce7c
BeardedPlatypus <mwtegelaers@gmail.com>
2020-12-31 00:42:49 +0100

    Add RecentProjectView

---
+++ Sprightly.Presentation.Views/RecentProjectView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sprightly.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for RecentProjectView.xaml
+    /// </summary>
+    public partial class RecentProjectView : UserControl
+    {
+        public RecentProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
