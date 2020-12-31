287d2ed53ed381846b6268b4eb5aaa5e543eb7bd
BeardedPlatypus <mwtegelaers@gmail.com>
2020-12-31 11:21:23 +0100

    Add NewProjectPageView xaml

---
+++ Sprightly.Presentation.Views/NewProjectPageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sprightly.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for NewProjectPageView.xaml
+    /// </summary>
+    public partial class NewProjectPageView : UserControl
+    {
+        public NewProjectPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
