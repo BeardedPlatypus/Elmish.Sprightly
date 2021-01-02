45323e66ad530738b70321449df65e7794ae4eaf
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-02 23:38:43 +0100

    Add Sprightly.Application project from the original Sprightly solution

---
+++ Sprightly.Application/Project.fs
@@ -0,0 +1,109 @@
+﻿namespace Sprightly.Application
+
+open Sprightly.Common
+open Sprightly.Domain
+
+/// <summary>
+/// The <see cref="Project"/> module contains all logic for interacting with
+/// projects, including retrieving recent projects, saving and loading existing
+/// projects and creating new projects.
+/// </summary>
+module public Project =
+    /// <summary>
+    /// The description of the LoadRecentProjects function.
+    /// </summary>
+    type public LoadRecentProjectsFunc = unit -> (RecentProject.T list) option
+
+    /// <summary>
+    /// The description of the SaveRecentProjects function.
+    /// </summary>
+    type public SaveRecentProjectsFunc = RecentProject.T list -> unit
+
+    /// <summary>
+    /// Load the recent projects.
+    /// </summary>
+    /// <param name="fLoadRecentProjects">
+    /// Function to load the recent projects.
+    /// </param>
+    /// <returns>
+    /// The list of <see cref="RecentProject"/> records describing the recent lists
+    /// ordered by date.
+    /// </returns>
+    let public loadRecentProjects (fLoadRecentProjects: LoadRecentProjectsFunc) () : (RecentProject.T list) option =
+        fLoadRecentProjects ()
+
+    /// <summary>
+    /// Update the stored recent project such that the specified 
+    /// <paramref name="recentProject"/> is moved to the top of the list.
+    /// </summary>
+    /// <param name="fLoadRecentProjects">
+    /// Function to load the recent projects.
+    /// </param>
+    /// <param name="fSaveRecentProjects">
+    /// Function to save the recent projects.
+    /// </param>
+    /// <param name="recentProject">
+    /// The recent project to move to the top of recent project.
+    /// </param>
+    /// <remarks>
+    /// If <paramref name="recentProject"/> exists within the recent 
+    /// projects, it is moved to the top. If it does not exist within
+    /// the recent project it is added as a new element to the top of
+    /// the recent projects.
+    /// </remarks>
+    let public moveProjectToTopOfRecentProjects (fLoadRecentProjects: LoadRecentProjectsFunc) 
+                                                (fSaveRecentProjects: SaveRecentProjectsFunc)
+                                                (recentProject: RecentProject.T) : unit =
+        fLoadRecentProjects () 
+        |> Option.defaultValue []
+        |> List.filter (fun x -> x.Id <> recentProject.Id)
+        |> (fun l -> recentProject :: l)
+        |> fSaveRecentProjects
+
+    /// <summary>
+    /// Function to retrieve texture paths from the solution file at the specified path.
+    /// </summary>
+    type public RetrieveTexturePathsFromSolutionFunc = Path.T -> (Texture.TextureDescription list) option
+
+    /// <summary>
+    /// Load the project at the given <paramref name="solutionFilePath"/>.
+    /// </summary>
+    /// <param name ="fRetrieveTexturePathsFromSolution"/>
+    /// Function to retrieve the relevant texture paths from the <paramref name="solutionFilePath"/>.
+    /// </param>
+    /// <param name="fRetrieveTextureData>
+    /// Function to retrieve the texture from the given texture path.
+    /// </param>
+    let public loadProject (fRetrieveTexturePathsFromSolution: RetrieveTexturePathsFromSolutionFunc)
+                           (fRetrieveTextureData: Texture.RetrieveTextureDataFunc)
+                           (fLoadTexture: Texture.LoadTextureFunc)
+                           (solutionFilePath: Path.T) : Project option =
+        
+        let texturePaths = fRetrieveTexturePathsFromSolution solutionFilePath
+
+        texturePaths |> Option.map (fun paths ->
+            let textureStore : Textures.Texture.Store = 
+                List.map fRetrieveTextureData paths
+                |> List.choose id
+                |> Textures.Texture.addTexturesToStore (Textures.Texture.emptyStore ())
+
+            for t in textureStore do fLoadTexture t
+
+            { TextureStore = textureStore
+              SolutionPath = solutionFilePath
+            })
+
+    /// <summary>
+    /// Function to write an empty solution file.
+    /// </summary>
+    type public WriteEmptySolutionFunc = Path.T -> unit
+
+    /// <summary>
+    /// Create a new empty solution file and corresponding file structure at
+    /// the specified <paramref name="slnFile"/>.
+    /// </summary>
+    /// <param name="fWriteEmptySolution">Function to write an empty solution file.</param>
+    /// <param name="slnFile">The path to the solution file to write.</param>
+    let public createNewProject (fWriteEmptySolution) 
+                                (slnFile: Path.T) : unit = 
+        fWriteEmptySolution slnFile
---
+++ Sprightly.Application/Texture.fs
@@ -0,0 +1,100 @@
+﻿namespace Sprightly.Application
+
+open Sprightly.Common
+open Sprightly.Domain
+
+/// <summary>
+/// The <see cref="Application.Texture"/> module contains all business
+/// logic related to interacting with textures.
+/// </summary>
+module public Texture =
+    /// <summary>
+    /// <see cref="TextureDescription"/> describes all information
+    /// related to loading a texture.
+    /// </summary>
+    type public TextureDescription = 
+        { Name : string 
+          Id : Textures.Texture.Id
+          Path : Path.T
+        }
+
+    /// <summary>
+    /// The function to retrieve all information of a texture from a given
+    /// <see cref="TextureDescription"/>.
+    /// <summary>
+    type public RetrieveTextureDataFunc = TextureDescription -> Textures.Texture.T option
+
+    /// <summary>
+    /// The function to retrieve the <see cref="MetaData.T"/> of a texture from a given
+    /// <see cref="Path.T"/>.
+    /// <summary>
+    type public RetrieveTextureMetaDataFunc = Path.T -> Textures.MetaData.T option
+
+    /// <summary>
+    /// The function to load the specified texture into memory.
+    /// </summary>
+    type public LoadTextureFunc = Textures.Texture.T -> unit
+
+    /// <summary>
+    /// The function to copy the texture defined in the <see cref="TextureCopyData"/>
+    /// and return the new location.
+    /// </summary>
+    type public CopyTextureIntoSolutionFunc = Path.T -> Path.T option
+
+    /// <summary>
+    /// The function to save the store to disk.
+    /// </summary>
+    type public SaveStoreFunc = Textures.Texture.Store -> unit
+
+    /// <summary>
+    /// Adds the texture at the specified <paramref name="texturePath"/> to the
+    /// specified the provided <paramref name="store"/> and return the id of 
+    /// the new texture and the new store.
+    /// </summary>
+    /// <param name="fCopyTextureIntoSolution">
+    /// The function to copy the provided texture path into the solution, and 
+    /// return the new texture path.
+    /// </param>
+    /// <param name="fRetrieveMetaData>
+    /// The function to retrieve the <see cref="Textures.MetaData.T"/> of a
+    /// texture.
+    /// </param>
+    /// <param name="fLoadTexture">
+    /// The function to load the <see cref="Textures.Texture.T"/> into memory.
+    /// </param>
+    /// <param name="texturePath">
+    /// The path outside of the sln directory from which the file should be copied.
+    /// </param>
+    /// <param name="store">
+    /// The current texture store of this Sprightly application.
+    /// </param>
+    /// <returns>
+    /// The id of the newly created texture and the new store.
+    /// <returns>
+    let public addNewTextureToStore (fCopyTextureIntoSolution: CopyTextureIntoSolutionFunc)
+                                    (fRetrieveMetaData: RetrieveTextureMetaDataFunc)
+                                    (fLoadTexture: LoadTextureFunc)
+                                    (fSaveStore: SaveStoreFunc)
+                                    (texturePath: Path.T)
+                                    (store: Textures.Texture.Store) : 
+                                    (Textures.Texture.Id * Textures.Texture.Store) option =
+        let slnTexturePath = fCopyTextureIntoSolution texturePath
+        if slnTexturePath.IsSome then
+            let metaData = fRetrieveMetaData slnTexturePath.Value
+            
+            if metaData.IsSome then 
+                let name = Path.nameWithoutExtension slnTexturePath.Value
+                let id = Textures.Texture.getUniqueId store name
+                let newTexture = 
+                    Textures.Texture.construct id name slnTexturePath.Value metaData.Value
+            
+                fLoadTexture newTexture
+            
+                let newStore = Textures.Texture.addTextureToStore store newTexture
+
+                fSaveStore newStore
+                Some (id, newStore)
+            else 
+                None
+        else 
+            None 
