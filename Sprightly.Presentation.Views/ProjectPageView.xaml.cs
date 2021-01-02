3d7f2389f9733864c44cd52453b48100391ec034
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-02 22:33:32 +0100

    Add Skeleton ProjectPageView

---
+++ Sprightly.Presentation.Views/ProjectPageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sprightly.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for ProjectPageView.xaml
+    /// </summary>
+    public partial class ProjectPageView : UserControl
+    {
+        public ProjectPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
