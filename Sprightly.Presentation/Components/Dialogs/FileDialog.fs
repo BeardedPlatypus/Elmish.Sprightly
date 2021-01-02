a39902691de004de2c8ffe2373c26700fcab88d0
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-02 13:24:35 +0100

    Add FileDialog to Presentation.Comoponents

---
+++ Sprightly.Presentation/Components/Dialogs/FileDialog.fs
@@ -0,0 +1,90 @@
+﻿namespace Sprightly.Presentation.Components.Dialogs
+
+open System.Threading
+open System.Windows
+
+open Elmish
+
+open Sprightly
+
+module public FileDialog =
+    let private configureProperty (optionalValue: 'T option) (propAssign: 'T -> unit) : unit =
+        if optionalValue |> Option.isSome then propAssign optionalValue.Value
+
+    let private configureDialogWith (config: FileDialogConfiguration)
+                                    (dialog: Microsoft.Win32.FileDialog) : unit =
+        configureProperty config.AddExtension 
+                          (fun b -> dialog.AddExtension <- b)
+        configureProperty config.CheckIfFileExists 
+                          (fun b -> dialog.CheckFileExists <- b)
+        configureProperty config.DereferenceLinks 
+                          (fun b -> dialog.DereferenceLinks <- b)
+        configureProperty config.Filter
+                          (fun f -> dialog.Filter <- f)
+        configureProperty config.FilterIndex
+                          (fun fi -> dialog.FilterIndex <- fi)
+        configureProperty config.InitialDirectory
+                          (fun p -> dialog.InitialDirectory <- p |> Common.Path.toString)
+        configureProperty config.RestoreDirectory
+                          (fun b -> dialog.RestoreDirectory <- b)
+        configureProperty config.Title
+                          (fun t -> dialog.Title <- t)
+        configureProperty config.ValidateNames
+                          (fun b -> dialog.ValidateNames <- b)
+
+    let private configureSaveDialogWith (config: FileDialogConfiguration) 
+                                        (dialog: Microsoft.Win32.SaveFileDialog) : Microsoft.Win32.SaveFileDialog =
+        dialog |> (configureDialogWith config)
+        dialog
+
+    let private configureOpenDialogWith (config: FileDialogConfiguration)
+                                        (dialog: Microsoft.Win32.OpenFileDialog) : Microsoft.Win32.OpenFileDialog =
+        dialog |> (configureDialogWith config)
+        configureProperty config.MultiSelect
+                          (fun p -> dialog.Multiselect <- p)
+        configureProperty config.ReadOnlyChecked
+                          (fun p -> dialog.ReadOnlyChecked <- p)
+        configureProperty config.ReadOnlyChecked
+                          (fun p -> dialog.ShowReadOnly <- p)
+
+        dialog
+
+    type public DialogType =
+        | Open
+        | Save
+
+    let private getDialog (dialogType: DialogType) (config: FileDialogConfiguration) : Microsoft.Win32.FileDialog =
+        match dialogType with 
+        | Save -> (Microsoft.Win32.SaveFileDialog () |> (configureSaveDialogWith config)) :> Microsoft.Win32.FileDialog
+        | Open -> (Microsoft.Win32.OpenFileDialog () |> (configureOpenDialogWith config)) :> Microsoft.Win32.FileDialog
+
+    let private showDialogAsync (toSuccessMsg: Common.Path.T -> 'Msg)
+                                (toCancelMsg: unit -> 'Msg)
+                                (guiContext: SynchronizationContext) 
+                                (dialogType : DialogType)
+                                (config: FileDialogConfiguration) =
+        async {
+            do! Async.SwitchToContext guiContext
+
+            let dlg = getDialog dialogType config
+            let isOk = dlg.ShowDialog ()
+            
+            if isOk.HasValue && isOk.Value then 
+                return dlg.FileName |> Common.Path.fromString |> toSuccessMsg
+            else 
+                return toCancelMsg ()
+        }
+
+    let public showDialogCmd (toSuccessMsg: Common.Path.T -> 'Msg)
+                             (toCancelMsg: unit -> 'Msg)
+                             (toErrMsg: exn -> 'Msg)
+                             (dialogType: DialogType)
+                             (config: FileDialogConfiguration) =
+        let asyncCmd () =
+            Application.Current.Dispatcher.Invoke(
+                fun () -> showDialogAsync toSuccessMsg 
+                                          toCancelMsg
+                                          SynchronizationContext.Current 
+                                          dialogType 
+                                          config)
+        Cmd.OfAsync.either asyncCmd () id toErrMsg
---
+++ Sprightly.Presentation/Components/Dialogs/FileDialogConfiguration.fs
@@ -0,0 +1,85 @@
+﻿namespace Sprightly.Presentation.Components.Dialogs
+
+open Sprightly
+
+/// <summary>
+/// <see cref="FileDialogConfiguration"/> defines the optional configuration
+/// options of a file dialog.
+/// </summary>
+type public FileDialogConfiguration (?addExtension: bool,
+                                     ?checkIfFileExists: bool,
+                                     ?dereferenceLinks: bool,
+                                     ?filter: string,
+                                     ?filterIndex: int,
+                                     ?initialDirectory: Common.Path.T,
+                                     ?multiSelect: bool,
+                                     ?readOnlyChecked: bool,
+                                     ?restoreDirectory: bool,
+                                     ?showReadOnly: bool,
+                                     ?title: string,
+                                     ?validateNames: bool) =
+    /// <summary>
+    /// Value indicating whether the dialog box automatically adds an 
+    /// extension to a file name if the user omits the extension.
+    /// </summary>
+    member val AddExtension: bool option = addExtension
+
+    /// <summary>
+    /// Value indicating whether the dialog box displays a warning if the user 
+    /// specifies a file name that does not exist.
+    /// </summary>
+    member val CheckIfFileExists: bool option = checkIfFileExists
+
+    /// <summary>
+    /// Value indicating whether the dialog box returns the location of the 
+    /// file referenced by the shortcut or whether it returns the location of 
+    /// the shortcut (.lnk).
+    /// </summary>
+    member val DereferenceLinks: bool option = dereferenceLinks
+
+    /// <summary>
+    /// The current file name filter string, which determines the choices that 
+    /// appear in the "Save as file type" or "Files of type" box in the dialog box.
+    /// </summary>
+    member val Filter: string option = filter
+
+    /// <summary>
+    ///  The index of the filter currently selected in the file dialog box.
+    /// </summary>
+    member val FilterIndex: int option = filterIndex
+
+    /// <summary>
+    /// The initial directory displayed by the file dialog box.
+    /// </summary>
+    member val InitialDirectory: Common.Path.T option = initialDirectory
+
+    /// <summary>
+    /// Value indicating whether the dialog box allows multiple files to be selected.
+    /// </summary>
+    member val MultiSelect: bool option = multiSelect
+
+    /// <summary>
+    /// Value indicating whether the read-only check box is selected.
+    /// </summary>
+    member val ReadOnlyChecked: bool option = readOnlyChecked
+
+    /// <summary>
+    /// value indicating whether the dialog box restores the directory to the 
+    /// previously selected directory before closing.
+    /// </summary>
+    member val RestoreDirectory: bool option = restoreDirectory
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the dialog box contains a read-only check box.
+    /// </summary>
+    member val ShowReadOnly: bool option = showReadOnly
+
+    /// <summary>
+    /// The file dialog box title.
+    /// </summary>
+    member val Title: string option = title
+
+    /// <summary>
+    /// Value indicating whether the dialog box accepts only valid Win32 file names.
+    /// </summary>
+    member val ValidateNames: bool option = validateNames
