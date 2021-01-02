af915969a785d4d681d205ba0bb260362011807c
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-02 22:30:14 +0100

    Refactor Domain.RecentProject

--- Sprightly.Domain/RecentProject.fs
+++ Sprightly.Domain/RecentProject.fs
@@ -3,11 +3,15 @@
 open System
 open Sprightly
 
-/// <summary>
-/// The <see cref="RecentProject"/> describes a recent project.
-/// </summary>
-type public RecentProject = 
-    { Path : Common.Path.T
-      LastOpened : DateTime
-    }
+module RecentProject = 
+    type public Id = | Id of int
 
+    type public Data = 
+        { Path : Common.Path.T
+          LastOpened : DateTime 
+        }
+
+    type public T = 
+        { Id : Id
+          Data : Data
+        }
