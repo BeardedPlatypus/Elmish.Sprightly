879bc8d894492f384958ca655710af3b0735a3aa
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-07 21:38:39 +0100

    Add SpriteToolBoxView to ProjectPageView

---
+++ Sprightly.Presentation.Views/Pages/ProjectPage/Toolboxes/SpriteToolbox/SpriteToolboxView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sprightly.Presentation.Views.Pages.ProjectPage.Toolboxes.SpriteToolbox
+{
+    /// <summary>
+    /// Interaction logic for SpriteToolboxView.xaml
+    /// </summary>
+    public partial class SpriteToolboxView : UserControl
+    {
+        public SpriteToolboxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
