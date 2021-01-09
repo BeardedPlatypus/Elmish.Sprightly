86b018747bcfb80cecfb724055e6d342e7cf791c
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-09 17:22:23 +0100

    Add TextureDetailView to Presentation.Views

---
+++ Sprightly.Presentation.Views/Pages/ProjectPage/Toolboxes/SpriteToolbox/TextureDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sprightly.Presentation.Views.Pages.ProjectPage.Toolboxes.SpriteToolbox
+{
+    /// <summary>
+    /// Interaction logic for TextureDetailView.xaml
+    /// </summary>
+    public partial class TextureDetailView : UserControl
+    {
+        public TextureDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
