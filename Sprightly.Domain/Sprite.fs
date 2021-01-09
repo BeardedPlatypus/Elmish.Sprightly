cc12a6c715a927eca116348d6ba0b0c259f3ed2e
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-09 13:26:08 +0100

    Extend Sprightly.Domain with Sprite

---
+++ Sprightly.Domain/Sprite.fs
@@ -0,0 +1,14 @@
+﻿namespace Sprightly.Domain
+
+module public Sprite =
+    type public Id = | Id of string * uint
+
+    type public Data = 
+        { Name : string
+        }
+
+    type public T =
+        { Id : Id 
+          Data : Data
+        }
+
