1fe6dddfca439886b19e2266fe56e0df53c12181
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-13 19:33:13 +0100

    Add TextureRenderStrategy

---
+++ Sprightly.Presentation.Views/Pages/ProjectPage/RenderStrategies/TextureRenderStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Sprightly.Common.KoboldLayer.Components;
+
+namespace Sprightly.Presentation.Views.Pages.ProjectPage.RenderStrategies
+{
+    public class TextureRenderStrategy : IRenderStrategy
+    {
+        private readonly string textureId;
+
+        public TextureRenderStrategy(string textureId)
+        {
+            this.textureId = textureId;
+        }
+
+        public void RenderFrame(IRenderer renderer)
+        {
+            renderer.RenderTexture(textureId);
+        }
+    }
+}
