dd83c7769763da4d9bc2b1e24411fe2f7e878852
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-13 19:31:00 +0100

    Add RenderStrategyType

---
+++ Sprightly.Presentation.Common/RenderStrategyType.fs
@@ -0,0 +1,10 @@
+﻿namespace Sprightly.Presentation.Common
+
+type public TextureRenderStrategyInfo(textureId: string) =
+    member public this.TextureId = textureId
+
+[<RequireQualifiedAccess>]
+type public RenderStrategyType =
+    | NoSelection
+    | Texture of TextureRenderStrategyInfo
+
