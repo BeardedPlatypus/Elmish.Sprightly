d93e808183c1214e0d8a5a93886394f0471182b3
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-13 17:08:24 +0100

    Add IRenderStrategy

---
+++ Sprightly.Common.KoboldLayer.Components/IRenderStrategy.cs
@@ -0,0 +1,15 @@
+﻿namespace Sprightly.Common.KoboldLayer.Components
+{
+    /// <summary>
+    /// <see cref="IRenderStrategy"/> defines the interface with which
+    /// a single render pass can be rendered.
+    /// </summary>
+    public interface IRenderStrategy
+    {
+        /// <summary>
+        /// Renders a frame.
+        /// </summary>
+        /// <param name="renderer">The renderer used to render the frame.</param>
+        void RenderFrame(IRenderer renderer);
+    }
+}
