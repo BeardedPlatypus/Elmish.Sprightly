a105e6a0556d39107e5dfed2fbc087627f6daa1c
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-25 21:21:21 +0100

    AB#223: Add Rectangle

---
+++ Sprightly.Domain/Rectangle.fs
@@ -0,0 +1,10 @@
+﻿namespace Sprightly.Domain
+
+module Rectangle =
+    [<RequireQualifiedAccess>]
+    type public T =
+        { x: int 
+          y: int
+          width: int
+          height: int
+        }
