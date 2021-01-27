982e272e2e46b83976287b36ed4f5e850a6dd99b
BeardedPlatypus <mwtegelaers@gmail.com>
2021-01-27 22:02:19 +0100

    Document Rectangle module D3DFMIQ-223

--- Sprightly.Domain/Rectangle.fs
+++ Sprightly.Domain/Rectangle.fs
@@ -1,6 +1,14 @@
 ﻿namespace Sprightly.Domain
 
+/// <summary>
+/// <see cref="Rectangle"/> defines the types and functions related to
+/// managing rectangles used within the rendering logic of Sprightly.
+/// </summary>
 module Rectangle =
+    /// <summary>
+    /// <see cref="Rectangle.T"/> defines a single rectangle consisting
+    /// of a width and height and a lower left corner defined by (x, y)
+    /// </summary>
     [<RequireQualifiedAccess>]
     type public T =
         { x: uint 
