15059d7169ad80885324e556d738d6c904409adb
BeardedPlatypus <mwtegelaers@gmail.com>
2021-03-10 23:14:29 +0100

    AB#227: Add docomentation to Sprightly.Persistence.AppData.fs

--- Sprightly.Persistence/AppData.fs
+++ Sprightly.Persistence/AppData.fs
@@ -2,6 +2,10 @@
 
 open Sprightly
 
+/// <summary>
+/// <see cref="AppData"/> defines the logic to read and write
+/// the appData to disk.
+/// </summary>
 module public AppData =
     [<RequireQualifiedAccess>]
     module BaseData = 
@@ -35,21 +39,45 @@ module public AppData =
     let private appDataFilePath = 
         Common.Path.combine appDataFolder (Common.Path.fromString fileName)
 
+    /// <summary>
+    /// Initialise a new AppData file on disk.
+    /// </summary>
     let public initialise () : unit =
         if not (Common.Path.exists appDataFilePath) then
             (BaseData.current |> BaseData.toDAR)
             |> Json.serialize
             |> Json.writeJsonString appDataFilePath
 
+    /// <summary>
+    /// Retrieve the field associated with the specified <paramref name="key"/> as type <typeparamref name="T">.
+    /// </summary>
+    /// <param name="key">The key to retrieve the associated data with.</param>
+    /// <returns>
+    /// The field value associated with the specified <paramref name="key"/>.
+    /// </returns>
     let public retrieveField<'T> (key: string) : Result<'T, exn> =
         Json.readJsonString appDataFilePath
         |> Json.queryKey<'T> key
 
+    /// <summary>
+    /// Retrieve the field associated with the specified <paramref name="key"/> as type <typeparamref name="T">.
+    /// If no value is associated with <paramref name="key"/> then <paramref name="defaultValue"/> will be returned.
+    /// </summary>
+    /// <param name="defaultValue">The default value to return if no key is defined.</param>
+    /// <param name="key">The key to retrieve the associated data with.</param>
+    /// <returns>
+    /// The field value associated with the specified <paramref name="key"/> if data exists otherwise <paramref name="defaultValue"/>.
+    /// </returns>
     let public retrieveFieldWithDefault<'T> (defaultValue: 'T) (key: string) : 'T =
         match retrieveField<'T> key with 
         | Ok v    -> v 
         | Error _ -> defaultValue
 
+    /// <summary>
+    /// Update the value associated with <paramref name="key"/> to the specified <paramref name="data"/>.
+    /// </summary>
+    /// <param name="key">The key to update.</param>
+    /// <param name="data">The new data associated with the key.</param>
     let public updateField<'T> (key: string) (data: 'T) : unit =    
         let newAppData = 
             Json.readJsonString appDataFilePath
